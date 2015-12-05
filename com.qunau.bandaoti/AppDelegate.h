6e68191bdda9c7b1bbf719562ee60942c577a02e
CaiYang <774164@qq.com>
2015-12-05 22:57:49 +0800

    Initial Commit

---
+++ com.qunau.bandaoti/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  com.qunau.bandaoti
+//
+//  Created by cy on 15/12/5.
+//  Copyright © 2015年 Qunau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ com.qunau.bandaoti/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  com.qunau.bandaoti
+//
+//  Created by cy on 15/12/5.
+//  Copyright © 2015年 Qunau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
