696143b53b6ff1a8bb04281c06aafa38ce1ef88b
CaiYang <774164@qq.com>
2015-12-14 22:37:55 +0800

    开始完善页面

--- com.qunau.bandaoti/MainTabBarController.m
+++ com.qunau.bandaoti/MainTabBarController.m
@@ -9,34 +9,50 @@
 #import "MainTabBarController.h"
 
 @interface MainTabBarController ()
-
+@property (nonatomic,retain) NSArray *tabNameArray;
+@property (nonatomic,retain) NSArray *tabImageArray;
+@property (nonatomic,retain) NSArray *tabSelectedImageArray;
 @end
 
 @implementation MainTabBarController
 
+#pragma mark 属性初始化
+
+- (NSArray *)tabNameArray {
+    return @[@"发现",@"旅途",@"心愿",@"我"];
+}
+
+- (NSArray *)tabImageArray {
+    return @[@"explore",@"trip",@"wish",@"self"];
+}
+
+- (NSArray *)tabSelectedImageArray {
+    return @[@"explore_active",@"trip_active",@"wish_active",@"self_active"];
+}
+
+#pragma mark life cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UIViewController *vc = [[UIStoryboard storyboardWithName:@"Explore" bundle:nil] instantiateInitialViewController];
-    [self setViewControllers:@[vc,vc,vc]];
+    UIViewController *explore = [[UIStoryboard storyboardWithName:@"Explore" bundle:nil] instantiateInitialViewController];
+    UIViewController *trip = [[UIStoryboard storyboardWithName:@"Trip" bundle:nil] instantiateInitialViewController];
+    UIViewController *wish = [[UIStoryboard storyboardWithName:@"Wish" bundle:nil] instantiateInitialViewController];
+    UIViewController *me = [[UIStoryboard storyboardWithName:@"Me" bundle:nil] instantiateInitialViewController];
+    [self setViewControllers:@[explore,trip,wish,me]];
     
-    
-    UITabBarController *qwe = [[UITabBarController alloc] ]
-}
+    for (int i = 0; i < self.viewControllers.count; i++) {
+        NSString *title = self.tabNameArray[i];
+        UIImage *tabImage = [UIImage imageNamed:self.tabImageArray[i]];
+        //暂时 没有设置 selectedImage  图片有问题？
+//        UIImage *tabSelectedImage = [UIImage imageNamed:self.tabSelectedImageArray[i]];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//        (self.viewControllers[i]).tabBarItem = [[UITabBarItem alloc] initWithTitle:title
+//                                                                             image:tabImage
+//                                                                     selectedImage:tabSelectedImage];
+        (self.viewControllers[i]).tabBarItem = [[UITabBarItem alloc] initWithTitle:title image:tabImage tag:i];
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    }
 }
-*/
 
 @end
